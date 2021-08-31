06161b832c8f9de62e9e6e30bb59d6927a1540ed
si-tm <kumomaya@gmail.com>
2021-09-01 00:12:07 +0900

    画面遷移

--- src/App.js
+++ src/App.js
@@ -3,6 +3,59 @@ import Amplify from 'aws-amplify';
 import { AmplifyAuthenticator, AmplifySignUp, AmplifySignOut } from '@aws-amplify/ui-react';
 import { AuthState, onAuthUIStateChange } from '@aws-amplify/ui-components';
 import awsconfig from './aws-exports';
+import MyPage from './MyPage';
+import SelectFile from './SelectFile';
+
+import {
+  HashRouter,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
+
+
+import { makeStyles, createMuiTheme, ThemeProvider } from '@material-ui/core/styles'; //追加
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+
+//style
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    height: '100%',
+    width: 800,
+    marginLeft: 'auto',
+    marginRight: 'auto',
+  }
+}))
+
+const theme = createMuiTheme({
+  palette: {
+    type: 'dark',
+    primary: {
+      main: '#1EA1F2',
+      contrastText: "#fff",
+    },
+    background: {
+      default: '#15202B',
+      paper: '#15202B',
+    },
+    divider: '#37444C',
+  },
+  overrides: {
+    MuiButton: {
+      color: 'white',
+    },
+  },
+  typography: {
+    fontFamily: [
+      'Arial', 
+    ].join(','),
+  },
+  status: {
+    danger: 'orange',
+  },
+});
 
 Amplify.configure(awsconfig);
 
@@ -17,11 +70,32 @@ const App = () => {
         });
     }, []);
 
+    const classes = useStyles(); //追加
+
   return authState === AuthState.SignedIn && user ? (
-      <div className="App">
-          <div>Hello, {user.username}</div>
-          <AmplifySignOut />
-      </div>
+      // <div className="App">
+      //     <div>Hello, {user.username}</div>
+      //     <AmplifySignOut />
+          
+      // </div>
+      <div className={classes.root} >
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <HashRouter>
+          <Switch>
+          <Route exact path='/' component={MyPage} />
+          <Route exact path='/mypage' component={MyPage} />
+          <Route exact path='/selectfile' component={SelectFile} />
+          <Redirect path="*" to="/" />
+          
+            {/* <Route exact path='/' component={AllPosts} />
+            <Route exact path='/global-timeline' component={AllPosts} />
+            <Route exact path='/:userId' component={PostsBySpecifiedUser}/>
+            <Redirect path="*" to="/" /> */}
+          </Switch>
+        </HashRouter>
+      </ThemeProvider>
+    </div>
     ) : (
       <AmplifyAuthenticator>
         <AmplifySignUp
