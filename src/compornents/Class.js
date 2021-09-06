60480f59329d08eb3d63411fd5ee033682697acd
Iinuma Mizuki <imiinz1331@gmail.com>
2021-09-06 22:02:07 +0900

    Classのmutation作成 RoomIDは渡せていない

--- src/compornents/Class.js
+++ src/compornents/Class.js
@@ -13,8 +13,10 @@ import {
     TextField,
     ListItemIcon,
   } from '@material-ui/core';
-  import {Auth, API, graphqlOperation } from 'aws-amplify';
-  import { useHistory } from 'react-router';
+import {Auth, API, graphqlOperation } from 'aws-amplify';
+import { useHistory } from 'react-router';
+import {createRoomTable} from '../graphql/mutations' //変更
+
 
 const drawerWidth = 240;
 
@@ -46,13 +48,64 @@ const useStyles = makeStyles(theme => ({
   
 export default function Class() {
     const [user] = React.useState();
+    const [text, setText] = React.useState('Please write.');
     const classes = useStyles();
     const history = useHistory();
+    const handleSubmit = (event) => {
+      const target = event.target;
+      event.preventDefault();
+      alert('入力内容: ' + target.value);
+      const newroom = API.graphql(
+        graphqlOperation(createRoomTable, {
+          input: {
+            ClassID: 'a8e5f441-4737-402e-a550-b4bc1fce5a2d',
+            RoomName: text.roomname,
+            OwnerUserID: '0003',
+            Comment: text.comment,
+          }
+        }))
+      console.log(newroom);
+      //const roomData = newroom.data.createRoomTable; // createしたクラス情報
+      //console.log(roomData);
+    };
     return (
         <React.Fragment>
         <div className={classes.root}>
           class</div>    
         <div>
+        <Button
+            variant="outlined"
+            onClick={() => {
+                            Auth.currentAuthenticatedUser().then((user) => {
+                            history.push('/');
+                            console.log("click");
+                            })
+                        }}>
+            マイページに戻る
+          </Button>
+
+          <label>
+            第何回：
+            <input type="text"
+              name="roomname"
+              value={text.roomname}
+              onChange={(e) => setText({...text, roomname: e.target.value})}/>
+          </label>
+          <br></br>
+          <label>
+            説明：
+            <input type="text"
+              name="comment"
+              value={text.comment}
+              onChange={(e) => setText({...text, comment: e.target.value})}/>
+          </label>
+          <br></br>
+          <Button
+            variant="outlined"
+            onClick={handleSubmit}>
+            部屋作成
+            </Button>
+            <br></br>
         <Button
             variant="outlined"
             onClick={() => {
